384e44dc1c6a0fc08718dbebe0085cbb83473891
Lindsay Trenton <108499545+lt353@users.noreply.github.com>
2025-11-19 11:49:10 -1000

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: '/Agilekioskapp/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
